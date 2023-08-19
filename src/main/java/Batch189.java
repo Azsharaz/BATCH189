1efe7337c8d48c52b23e2126ef0bc93a1112b62d
Selen <selenoz97@gmail.com>
2023-08-19 23:24:51 +0300

    Version-5(BUG FIX)

--- src/main/java/Batch189.java
+++ src/main/java/Batch189.java
@@ -12,18 +12,6 @@ public class Batch189 {
         System.out.println("Version-2");
 
 
-        //VERSION 3
-
-        System.out.println("Version-3");
-
-
-
-        // VERSION 4
-
-        System.out.println("Version-4");
-
-
-
 
     }
 }
